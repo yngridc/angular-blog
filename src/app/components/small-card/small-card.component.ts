d3e1a35c65351eff583e55ac3ae0ece4afa5e373
Yngrid <dhyks1310@gmail.com>
2023-10-21 18:38:35 -0300

    alterações no projeto

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent{
 
+@Input()
+photoCover:string =""
+@Input()
+cardTitle:string =""
+  // ngOnInit(): void {
+
+  // } 
 }
